d5ee727231118926726dab1f485f7664f70dbe2b
Naik Mubashir <naikmubashir095@gmail.com>
2025-01-28 09:02:26 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button, Container } from "@chakra-ui/react"
+
+
+function App() {
+
+  return (
+   <Container maxWidth="620px">
+   <Button>Hello</Button>
+   <h1>Helllo world </h1>
+   <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci perferendis vel sed maxime, vero dolore aperiam quae at fuga, magni ipsa corporis totam nisi id modi sunt, ducimus esse distinctio!</h1>
+   </Container>
+  )
+}
+
+export default App
