6d8bdb272a14012970e8ef93a2c2353db056f209
Naik Mubashir <naikmubashir095@gmail.com>
2025-02-17 08:03:45 +0530

    Update the ice in the veins

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -62,6 +62,10 @@ export const loginUser= async(req,res)=>{
         if (!user || !isPasswordCorrect) {
             return res.status(400).json({error:"Invalid username or password"})
         }
+        if (user.isFrozen) {
+			user.isFrozen = false;
+			await user.save();
+		}
         generateTokenAndSetCookie(user._id,res);
         res.status(200).json({_id:user._id, name:user.name, email:user.email, username:user.username, bio:user.bio, profilePic:user.profilePic});  
         
@@ -164,3 +168,49 @@ export const updateUser = async (req, res) => {
         console.log('Error while updating the user')
     }
 }
+
+
+
+export const getSuggestedUsers = async (req, res) => {
+	try {
+		// exclude the current user from suggested users array and exclude users that current user is already following
+		const userId = req.user._id;
+
+		const usersFollowedByYou = await User.findById(userId).select("following");
+
+		const users = await User.aggregate([
+			{
+				$match: {
+					_id: { $ne: userId },
+				},
+			},
+			{
+				$sample: { size: 10 },
+			},
+		]);
+		const filteredUsers = users.filter((user) => !usersFollowedByYou.following.includes(user._id));
+		const suggestedUsers = filteredUsers.slice(0, 4);
+
+		suggestedUsers.forEach((user) => (user.password = null));
+
+		res.status(200).json(suggestedUsers);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+
+export const freezeAccount = async (req, res) => {
+	try {
+		const user = await User.findById(req.user._id);
+		if (!user) {
+			return res.status(400).json({ error: "User not found" });
+		}
+
+		user.isFrozen = true;
+		await user.save();
+
+		res.status(200).json({ success: true });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
