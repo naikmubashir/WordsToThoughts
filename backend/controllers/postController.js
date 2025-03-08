a1e5a32d7162cbc290faf95ad2bb9c11252a07bd
Naik Mubashir <naikmubashir095@gmail.com>
2025-03-09 02:02:58 +0530

    Fix bugs

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,4 +1,4 @@
-import User from "../models/UserModel.js";
+import User from "../models/userModel.js";
 import Post from "../models/postModel.js";
 import {v2 as cloudinary} from 'cloudinary'
 export const createPost= async (req,res)=>{
